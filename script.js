0a586a2456db05b88b1d310742aa37877df034d3
Blatahead <81589270+Blatahead@users.noreply.github.com>
2024-03-18 01:03:25 +0100

    tree icons href

--- script.js
+++ script.js
@@ -5,34 +5,6 @@ var newHeightDivHistoryText = 100-heightDivHistoryTitle+'vh';
 document.getElementById("history-text").style.height = newHeightDivHistoryText;
 
 
-const downArrowProducts = document.getElementById("down-arrow-to-products");
-
-downArrowProducts.addEventListener("click", function () {
-	window.scrollBy({
-		top: window.innerHeight,
-		behavior: "smooth"
-	});
-});
-const downArrowTeam = document.getElementById("down-arrow-to-team");
-
-downArrowTeam.addEventListener("click", function () {
-	window.scrollBy({
-		top: window.innerHeight,
-		behavior: "smooth"
-	});
-});
-const downArrowHistory = document.getElementById("down-arrow-to-history");
-
-downArrowHistory.addEventListener("click", function () {
-	window.scrollBy({
-		top: window.innerHeight,
-		behavior: "smooth"
-	});
-});
-
-
-
-
 // Gérer l'apparition de la team
 const observer = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
