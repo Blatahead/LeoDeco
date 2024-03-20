7f51970f993523c31a67c04e12da8b6c6ebee7ee
Blatahead <81589270+Blatahead@users.noreply.github.com>
2024-03-20 12:17:19 +0100

    try commit

--- app.js
+++ app.js
@@ -2,7 +2,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 import { getDatabase, ref, set, child } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
 
-const firebaseConfig = window.firebaseConfig;
+const firebaseConfig = {
+	authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
@@ -13,7 +21,7 @@ document.getElementById("submitButton").addEventListener('click', function(){
     const date = new Date();
 	const currentDate = `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
 
-	set(ref(db, 'user/' + document.getElementById('firstName').value + " " +document.getElementById('lastName').value+ " "+document.getElementById('postalCode').value),
+	set(ref(db, 'users/' + document.getElementById('firstName').value + " " +document.getElementById('lastName').value+ " "+document.getElementById('postalCode').value),
 	{
 		prenom : document.getElementById('firstName').value,
 		nom : document.getElementById('lastName').value,
